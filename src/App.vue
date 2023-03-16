aacb3b318bdbffc01a3dbb44e262d6c311eb9f50
dannyp6611 <dannyjp6611@gmail.com>
2023-03-16 17:34:35 +0000

    added favicon icon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,6 @@
 <template>
   <div
-    class="
-      w-full
-      min-h-screen
-      font-spartan
-      bg-body-bg bg-body-pattern bg-cover bg-repeat
-    "
+    class="w-full min-h-screen font-spartan bg-body-bg bg-body-pattern bg-cover bg-repeat"
   >
     <Navbar
       @tab-changed="setPlanetInfo"
@@ -23,16 +18,16 @@
 </template>
 
 <script>
-import Navbar from "@/components/Navbar.vue";
-import json from "../data.json";
-import { ref } from "@vue/reactivity";
+import Navbar from '@/components/Navbar.vue';
+import json from '../data.json';
+import { ref } from '@vue/reactivity';
 
 export default {
   components: { Navbar },
   setup() {
     const planets = ref(json);
     const planet = ref(planets.value[2]);
-    const currentTab = ref("overview");
+    const currentTab = ref('overview');
 
     const setPlanet = (name) => {
       planet.value = planets.value.find((planet) => planet.name === name);
@@ -47,6 +42,6 @@ export default {
 };
 </script>
 
-<style lang="sass">
+<style>
 @import url('https://fonts.googleapis.com/css2?family=Antonio:wght@200;300;400;500;600;700&family=Spartan:wght@200;400;700&display=swap');
 </style>
